a2fcca15b6291e17a204b6b825b15e1159591360
Vignesh Jagannadhan <vigneshjagannadhan1998@gmail.com>
2020-11-18 22:09:49 +0530

    any

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let count=0;
 let marks = 1;
 let jcount=0;
-
+function linker(){
+    let val1 = parseInt(document.getElementById("centered").innerHTML);
+    val=val1+1;
+    console.log(val);
+}
+setInterval(function(){location.href='qes'+val+'.html';return false;},10000);
 function right(){
     document.getElementById('answer').innerHTML="Correct Answer!"
     count+=1;
@@ -11,7 +16,7 @@ function right(){
         document.getElementById("option3").removeAttribute("onClick");
         document.getElementById("option4").removeAttribute("onClick");
         document.getElementById("option2").removeAttribute("onClick");
-        
+        setInterval(function(){location.href='qes'+val+'.html';return false;},3000);
     }
 };
 
@@ -27,5 +32,6 @@ function wrong(){
         document.getElementById("option3").removeAttribute("onClick");
         document.getElementById("option4").removeAttribute("onClick");
         document.getElementById("option2").removeAttribute("onClick");
+        setInterval(function(){location.href='qes'+val+'.html';return false;},3000);
     }
 };
---
+++ sripts.js
@@ -0,0 +1,6 @@
+function linker(){
+    let val = parseInt(document.getElementById("centered").innerHTML);
+    let link ='qes'+(val+1)+'.html';
+    console.log(link)
+    
+}function nevw(){location.href="qes4.html"};
